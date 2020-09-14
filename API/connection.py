fadbd0544852d274e42c3ecb2861110a41ca9fab
Ryder Stanley <ryderstanley2007@gmail.com>
2020-09-14 14:50:26 -0600

    added a test

--- API/connection.py
+++ API/connection.py
@@ -1,24 +1,44 @@
 # connection.py
 import os
 
+# imports discord api
 import discord
 from dotenv import load_dotenv
 
+# loads the api 
 load_dotenv()
+
+# sets the default token and guild
 TOKEN = os.getenv('YOKbMQoAiJNuNqH5-iQC29MhpPhUdPsh')
 GUILD = os.getenv('https://discord.gg/JaW49Q')
 
+# sets default client to discord main servers
 client = discord.Client()
 
+
+# creates a new client event 
 @client.event
+# creates a new function that triggers when the bot is loaded on guild
 async def on_ready():
-    for guild in client.guilds:
+    # enters a loop that checks if the bot is on the correct guild
+	for guild in client.guilds:
         if guild.name == GUILD:
             break
 
+	# if it is on the right server it prints out a connection message
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+    
+# TEST: Will Print Out Message if it works correctly    
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
+
+# runs the bot on our discord guild
 client.run(YOKbMQoAiJNuNqH5-iQC29MhpPhUdPsh)
